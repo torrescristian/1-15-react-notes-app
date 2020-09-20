8f127a45e3c6e73ac0bcc696e48641bb5cea367b
Cristian Torres <cristiantomastorres@gmail.com>
2020-09-20 15:54:48 -0300

    feat: github page

---
+++ docs/precache-manifest.c21e1a9c68eb31a7214fe6dae881241d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5407d561a2619a2c847bf03d01d47345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ab107ceeead46880f2f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c5a20f00cfcdec88d5c2",
+    "url": "/static/js/2.a0b1718e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a0b1718e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ab107ceeead46880f2f",
+    "url": "/static/js/main.67cd59ac.chunk.js"
+  },
+  {
+    "revision": "01ff5abfa2aca608c6fc",
+    "url": "/static/js/runtime-main.6ee577dc.js"
+  }
+]);
